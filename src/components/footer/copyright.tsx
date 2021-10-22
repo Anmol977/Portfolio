e187f2bdb036d0b025a0f76ea1995d3e0ac868cd
Anmol Chauhan <anmolchauhan977@gmail.com>
2021-10-22 19:38:23 +0530

    footer added

--- src/components/footer/copyright.tsx
+++ src/components/footer/copyright.tsx
@@ -1,8 +1,7 @@
 import { Text, TextProps } from '@chakra-ui/layout'
-import * as React from 'react'
 
 export const Copyright = (props: TextProps) => (
     <Text fontSize="sm" {...props}>
-        &copy; {new Date().getFullYear()} Envelope, Inc. All rights reserved.
+        &copy; {new Date().getFullYear()}. All rights reserved.
     </Text>
 )
